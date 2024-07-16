de6333009dc637dcc5cf61d87e835b4b13082449
oriori770 <oriori770@gmail.com>
2024-07-16 10:26:51 +0300

    remuve trash

--- MyDBForTest/Form1.cs
+++ MyDBForTest/Form1.cs
@@ -58,11 +58,7 @@ namespace MyDBForTest
             }
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
 
-
-        }
 
         private void button1_Click(object sender, EventArgs e)
         {
